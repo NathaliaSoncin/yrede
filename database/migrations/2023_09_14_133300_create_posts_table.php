1f029fe1429c93f171b97e3818d1d704c8b20502
NathaliaSoncin <nathalia.camargo@fatec.sp.gov.br>
2023-09-14 11:02:57 -0300

    tabela usuario e post

--- database/migrations/2023_09_14_133300_create_posts_table.php
+++ database/migrations/2023_09_14_133300_create_posts_table.php
@@ -1,25 +1,21 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
-{
+{ 
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('usuario');
-            $table->string('foto');
-            $table->string('bio')->nullable();
-            $table->string('nome');
-            $table->string('email')->unique();
-            $table->string('senha');
-            $table->rememberToken();
+            $table->foreignIdFor(User::class)->constrained(); //chave estrangeira de usuários
+            $table->string('mensagem');
             $table->timestamps();
         });
     }
@@ -29,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 };
