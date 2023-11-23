ecdc875e3019dc20ef5a10c0cf54438f15362635
NathaliaSoncin <nathysoncin@gmail.com>
2023-11-23 11:19:16 -0300

    trabalho

--- resources/views/criaPost.blade.php
+++ resources/views/criaPost.blade.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-theme="pastel">
 <head>
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nova Postagem</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@4.4.4/dist/full.min.css" rel="stylesheet" type="text/css" />
+<script src="https://cdn.tailwindcss.com"></script>
 
 </head>
 <body>
@@ -17,12 +20,13 @@
 
     <textarea name="mensagem" id="mensagem" cols="30" rows="10"></textarea>
 
-    <button type="submit">Postar</button>
-
+    
+<button type="submit" class="btn btn-primary">Postar</button>
     </form>
 
 </body>
 
 </html>
+
 
 
